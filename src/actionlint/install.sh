fbcecd5dc30f874990a3de5dac80642bfb94f6fb
Jason Garber <jason.garber@cargosense.com>
2024-02-28 13:43:30 -0500

    Address shellcheck error

--- src/actionlint/install.sh
+++ src/actionlint/install.sh
@@ -6,7 +6,7 @@ ACTIONLINT_VERSION="${VERSION:-"latest"}"
 INSTALL_PATH="${INSTALLPATH:-"/usr/local/bin"}"
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+  printf -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
   exit 1
 fi
 
