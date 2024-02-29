d10303d1998e61d3532b1bc148996e6c2e9290eb
Jason Garber <jason.garber@cargosense.com>
2024-02-29 20:24:48 +0000

    🤷🏻‍♂️

--- src/shellcheck/install.sh
+++ src/shellcheck/install.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
 SHELLCHECK_VERSION="${VERSION:-"os-provided"}"
 INSTALL_PATH="${INSTALLPATH:-"/usr/local/bin"}"
 
-if [[ "$(id -u)" -ne 0 ]]; then
+if [ "$(id -u)" -ne 0 ]; then
   printf 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
   exit 1
 fi
 
 
-if [[ "${SHELLCHECK_VERSION}" = "os-provided" ]]; then
+if [ "${SHELLCHECK_VERSION}" = "os-provided" ]; then
   apt update --yes
   apt install --no-install-recommends --yes shellcheck
 
@@ -27,7 +27,7 @@ if ! type curl >/dev/null 2>&1; then
   curl_installed="true"
 fi
 
-if [[ "${SHELLCHECK_VERSION}" = "latest" ]]; then
+if [ "${SHELLCHECK_VERSION}" = "latest" ]; then
   SHELLCHECK_VERSION="$(curl -s --head https://github.com/koalaman/shellcheck/releases/latest | sed -nr 's/location:.*\/v(.+)/\1/ip' | tr -d '\r')"
 fi
 
@@ -47,6 +47,6 @@ url="https://github.com/koalaman/shellcheck/releases/download/v${SHELLCHECK_VERS
 
 curl -sSL "${url}" | tar --strip-components=1 -Jxvf - -C "${INSTALL_PATH}" "shellcheck-v${SHELLCHECK_VERSION}/shellcheck"
 
-if [[ -n "${curl_installed}" ]]; then
+if [ -n "${curl_installed}" ]; then
   apt purge curl --autoremove --yes
 fi
--- test/shellcheck/shellcheck-version.sh
+++ test/shellcheck/shellcheck-version.sh
@@ -11,7 +11,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 # check <LABEL> <cmd> [args...]
-check "version" bash -c "shellcheck --version | grep 0.7.0"
+check "version" bash -c "shellcheck --version | grep 0.8.0"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
