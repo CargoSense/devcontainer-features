b8a1329900efe153260c1cb35f09d3dcbf04b1e3
Jason Garber <jason.garber@cargosense.com>
2024-02-29 14:48:27 +0000

    Ignore external source

--- test/shellcheck/test.sh
+++ test/shellcheck/test.sh
@@ -34,6 +34,7 @@ set -e
 # Optional: Import test library bundled with the devcontainer CLI
 # See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
 # Provides the 'check' and 'reportResults' commands.
+# shellcheck source=/dev/null
 source dev-container-features-test-lib
 
 # Feature-specific tests
