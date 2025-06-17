b20ee2d6f02910238c1ec8adac7563058409dd75
Jason Garber <jason.garber@cargosense.com>
2025-06-17 10:05:00 -0400

    Fix spelling mistake

--- src/apt-packages/install.sh
+++ src/apt-packages/install.sh
@@ -5,6 +5,6 @@ set -e
 apt update
 
 # shellcheck disable=SC2154
-apt install --no-install-recomends --yes "${PACKAGES}"
+apt install --no-install-recommends --yes "${PACKAGES}"
 
 rm -rf /var/lib/apt/lists /var/cache/apt/archives
