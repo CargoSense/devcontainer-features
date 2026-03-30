577b51cdd1c677500e16ea4ecb320a0fe97cc786
Jason Garber <jason.garber@cargosense.com>
2026-03-30 13:39:05 +0000

    Fix path to Node.js executable in test

--- test/node/test.sh
+++ test/node/test.sh
@@ -8,7 +8,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 check "version" bash -c "node --version | grep -E 'v25\..+'"
-check "which node" bash -c "which node | grep /usr/bin/node"
+check "which node" bash -c "which node | grep /usr/local/share/node/bin/node"
 
 # Report result
 reportResults
